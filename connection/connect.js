ea07c8b46e37b1e143c529b6d23e31e09683a3a7
Thillai22 <thillaiags@gmail.com>
2022-10-17 23:51:37 +0530

    first commit

---
+++ connection/connect.js
@@ -0,0 +1,22 @@
+const mongoose = require('mongoose');
+
+const connection = async () =>{
+    try{
+
+        const res = await mongoose.connect(
+          "mongodb+srv://admin:admin@onlineshopping.z6bz2.mongodb.net/?retryWrites=true&w=majority",
+          {
+            useNewUrlParser: true,
+            useUnifiedTopology: true
+          
+          }
+        );
+        if(res){
+            console.log("success in connection to db");
+        }
+    }
+    catch(err){
+        console.log(err);
+    }
+}
+module.exports = connection;
